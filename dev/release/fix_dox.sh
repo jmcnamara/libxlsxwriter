fbdd2800fa2504efaebf106a16dfac979d6c2dec
John McNamara <jmcnamara@cpan.org>
2020-09-06 02:19:13 +0100

    Updated to doxygen 1.8.20.

--- dev/release/fix_dox.sh
+++ dev/release/fix_dox.sh
@@ -2,8 +2,6 @@
 
 # Perform some minor clean-ups/fixes to the docs.
 
-perl -i -pe "s/Related&#160;Pages/Contents/" html/*.html
-perl -i -pe "s/Related Pages/Contents/"      html/*.html
 perl -i -pe "s/_page/_8h/"                   html/pages.html
 perl -i ../dev/release/fix_example_docs.pl   html/examples.html
-
+cp menudata.js html
