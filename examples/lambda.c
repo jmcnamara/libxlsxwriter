b3059c805d7e6edd2f14030a98c1b6c28d307363
John McNamara <jmcnamara@cpan.org>
2022-12-28 12:33:50 +0000

    Update docs/example for lambda function.

--- examples/lambda.c
+++ examples/lambda.c
@@ -1,7 +1,6 @@
 /*
  * An example of using the new Excel LAMBDA() function with the libxlsxwriter
- * library. Note, this function is only currently available if you are
- * subscribed to the Microsoft Office Beta Channel program.
+ * library.
  *
  * Copyright 2014-2021, John McNamara, jmcnamara@cpan.org
  *
@@ -14,15 +13,11 @@ int main() {
     lxw_workbook  *workbook  = workbook_new("lambda.xlsx");
     lxw_worksheet *worksheet = workbook_add_worksheet(workbook, NULL);
 
-    worksheet_write_string(worksheet, 0, 0,
-                           "Note: Lambda functions currently only work with "
-                           "the Beta Channel versions of Excel 365", NULL);
-
     /* Note that the formula name is prefixed with "_xlfn." and that the
      * lambda function parameters are prefixed with "_xlpm.". These prefixes
      * won't show up in Excel.
      */
-    worksheet_write_dynamic_formula(worksheet, CELL("A2"),
+    worksheet_write_dynamic_formula(worksheet, CELL("A1"),
                                     "=_xlfn.LAMBDA(_xlpm.temp, (5/9) * (_xlpm.temp-32))(32)",
                                     NULL);
 
@@ -31,7 +26,7 @@ int main() {
                          "ToCelsius",
                          "=_xlfn.LAMBDA(_xlpm.temp, (5/9) * (_xlpm.temp-32))");
 
-    worksheet_write_dynamic_formula(worksheet, CELL("A3"), "=ToCelsius(212)", NULL);
+    worksheet_write_dynamic_formula(worksheet, CELL("A2"), "=ToCelsius(212)", NULL);
 
 
     workbook_close(workbook);
