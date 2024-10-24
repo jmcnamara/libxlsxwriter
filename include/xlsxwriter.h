7ba204a82c3aa3940ae6f2dd89b3555b14f0f4c9
John McNamara <jmcnamara@cpan.org>
2024-10-24 23:47:44 +0100

    Prep for release 1.1.9.

--- include/xlsxwriter.h
+++ include/xlsxwriter.h
@@ -19,8 +19,8 @@
 #include "xlsxwriter/format.h"
 #include "xlsxwriter/utility.h"
 
-#define LXW_VERSION "1.1.8"
-#define LXW_VERSION_ID 118
+#define LXW_VERSION "1.1.9"
+#define LXW_VERSION_ID 119
 #define LXW_SOVERSION "7"
 
 #endif /* __LXW_XLSXWRITER_H__ */
