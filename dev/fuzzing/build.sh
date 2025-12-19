58ba28d2660d11a07a2d9a561cbd7206d644d6fa
Arthur Chan <arthur.chan@adalogics.com>
2025-12-19 07:15:21 +0000

    oss-fuzz: Add unit test build
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- dev/fuzzing/build.sh
+++ dev/fuzzing/build.sh
@@ -3,7 +3,7 @@ cd $SRC/libxlsxwriter
 printenv
 
 mkdir -p build
-cmake -S . -B build -DBUILD_FUZZERS=ON && cmake --build build --target install
+cmake -S . -B build -DBUILD_FUZZERS=ON -DBUILD_TESTS=ON && cmake --build build --target install
 
 # Build the corpus using the existing xlsx files in the source
 mkdir -p corpus
